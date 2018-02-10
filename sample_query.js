00c4422e3e62880be00a5c3b27dcf6bee7d425a8
Krissada Ingarn <theball_kyo@hotmail.com>
2018-02-10 14:30:45 +0700

    Add simple query

--- sample_query.js
+++ sample_query.js
@@ -1,20 +1,23 @@
 // Group netflow
-db.sdn_test2.netflow.aggregate([
+db.getCollection('netflow').aggregate([
     // Match stage
     {
         $match: {
             first_switched: {
-                $lte: Date("2016-05-16T04:18:00.000Z")
-            }
+                $lte: ISODate("2016-05-16T04:20:00.000Z"),
+                $gte: ISODate("2016-05-16T04:18:00.000Z")
+            },
+            l4_dst_port: 443
         }
     },
     {
         $group: {
             _id: {
                 "ipv4_dst_addr": "$ipv4_dst_addr",
-                "ipv4_dst_port": "$ipv4_dst_port",
-                "l4_src_port": "$l4_src_port",
-                "l4_dst_port": "$l4_dst_port"
+                "ipv4_src_addr": "$ipv4_src_addr",
+                 "l4_dst_port": "$l4_dst_port",
+                 "l4_src_port": "$l4_src_port",
+//                "first_switched": "$first_switched"
             },
             total_in_bytes: { $sum: "$in_bytes" },
             total_in_pkts: { $sum: "$in_pkts" }
@@ -22,8 +25,27 @@ db.sdn_test2.netflow.aggregate([
     },
     {
         $sort: {
-            total_in_bytes: -1,
-            total_in_pkts: -1
+            total_in_bytes: -1
+//             total_in_pkts: -1
         }
-    }
+    },
+    {
+        $limit: 10
+    },
+    {
+            "$group": {
+                "_id": null,
+                "total_flow":
+                    {
+                        "$sum": 1
+                    },
+                "data": {
+                    "$push": {
+                        "_id": "$_id",
+                        "total_in_bytes": "$total_in_bytes",
+                        "total_in_pkts": "$total_in_pkts"
+                    }
+                }
+            }
+        }
 ])
