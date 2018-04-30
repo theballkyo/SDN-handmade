69abef84b25b72b8d6f397f175f2c29fa9f9e06d
theballkyo <theballkyo@gmail.com>
2018-04-30 14:01:39 +0700

    Create sample network graph

---
+++ frontend/pages/graph.vue
@@ -0,0 +1,12 @@
+<template>
+  <network-graph/>
+</template>
+
+<script>
+import NetworkGraph from '@/components/NetworkGraph.vue'
+export default {
+  components: {
+    NetworkGraph
+  }
+}
+</script>
